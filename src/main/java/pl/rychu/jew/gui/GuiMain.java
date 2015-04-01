f75ddb77e7d55c1048e52bedee723b8229482d76
rycho <rycho@joasia>
2015-04-01 07:09:53 +0200

    move code from GuiMain to swing thread

--- src/main/java/pl/rychu/jew/gui/GuiMain.java
+++ src/main/java/pl/rychu/jew/gui/GuiMain.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 import pl.rychu.jew.LogFileAccess;
 
@@ -12,24 +13,26 @@ import pl.rychu.jew.LogFileAccess;
 public class GuiMain {
 
 	public static void main(final String... args) throws InterruptedException {
-		// TODO invoke later
-		final LogFileAccess logFileAccess
-		 = LogFileAccess.create("/home/rycho/Pulpit/server.log");
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				final LogFileAccess logFileAccess
+				 = LogFileAccess.create("/home/rycho/Pulpit/server.log");
 
-		Thread.sleep(1000L);
+				final JFrame mainFrame = new JFrame("jew");
 
-		final JFrame mainFrame = new JFrame("jew");
+				final LogViewPanel logViewPanel = LogViewPanel.create(logFileAccess);
 
-		final LogViewPanel logViewPanel = LogViewPanel.create(logFileAccess);
+				final JScrollPane scrollPane = new JScrollPane(logViewPanel);
+				scrollPane.setPreferredSize(new Dimension(600, 600));
+				mainFrame.add(scrollPane);
 
-		final JScrollPane scrollPane = new JScrollPane(logViewPanel);
-		scrollPane.setPreferredSize(new Dimension(600, 600));
-		mainFrame.add(scrollPane);
-
-		mainFrame.pack();
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setTitle("Java log viEW");
-		mainFrame.setVisible(true);
+				mainFrame.pack();
+				mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				mainFrame.setTitle("Java log viEW");
+				mainFrame.setVisible(true);
+			}
+		});
 	}
 
 }
