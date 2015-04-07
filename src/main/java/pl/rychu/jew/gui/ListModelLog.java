d539212f886fc2aaf5f48af940cb52d393b5304c
rycho <rycho@joasia>
2015-04-07 06:35:44 +0200

    extract ListModelLog from LogViewPanel

---
+++ src/main/java/pl/rychu/jew/gui/ListModelLog.java
@@ -0,0 +1,135 @@
+package pl.rychu.jew.gui;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.swing.AbstractListModel;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pl.rychu.jew.LogFileAccess;
+import pl.rychu.jew.LogFileListener;
+import pl.rychu.jew.LogLineFull;
+
+
+
+public class ListModelLog extends AbstractListModel<LogLineFull>
+ implements LogFileListener {
+
+	private static final long serialVersionUID = 5990060914470736065L;
+
+	private final LogFileAccess logFileAccess;
+
+	private final AtomicBoolean mustNotifyReset = new AtomicBoolean(false);
+
+	private final AtomicBoolean mustNotifyInsert = new AtomicBoolean(false);
+
+	private final List<CyclicModelListener> listeners
+	 = new CopyOnWriteArrayList<>();
+
+	// ------------------
+
+	private ListModelLog(final LogFileAccess logFileAccess) {
+		this.logFileAccess = logFileAccess;
+	}
+
+	public static ListModelLog create(final LogFileAccess logFileAccess
+	 , final LogViewPanel logViewPanel, final InfoPanel infoPanel) {
+		final ListModelLog result = new ListModelLog(logFileAccess);
+
+		logFileAccess.addLogFileListener(result);
+
+		result.addCyclicModelListener(infoPanel);
+		result.addCyclicModelListener(logViewPanel);
+
+		new Thread(result.new ModNotifier()).start();
+
+		return result;
+	}
+
+	public void addCyclicModelListener(final CyclicModelListener lsn) {
+		listeners.add(lsn);
+	}
+
+	public void removeCyclicModelListener(final CyclicModelListener lsn) {
+		listeners.remove(lsn);
+	}
+
+	@Override
+	public int getSize() {
+		return (int)logFileAccess.size();
+	}
+
+	@Override
+	public LogLineFull getElementAt(final int index) {
+		return logFileAccess.getFull(index);
+	}
+
+	@Override
+	public void linesAdded() {
+		mustNotifyInsert.set(true);
+	}
+
+	@Override
+	public void fileWasReset() {
+		mustNotifyReset.set(true);
+	}
+
+	// ------
+
+	private class ModNotifier implements Runnable {
+
+		private final Logger log = LoggerFactory.getLogger(ModNotifier.class);
+
+		// -------
+
+		ModNotifier() {
+			super();
+		}
+
+		// -------
+
+		@Override
+		public void run() {
+			while (!Thread.interrupted()) {
+				if (mustNotifyReset.getAndSet(false)) {
+					log.debug("scheduling file reset");
+					SwingUtilities.invokeLater(new Runnable() {
+						@Override
+						public void run() {
+							fireIntervalRemoved(this, 0, Integer.MAX_VALUE>>1);
+							for (final CyclicModelListener listener: listeners) {
+								listener.listReset();
+							}
+						}
+					});
+				}
+
+				if (mustNotifyInsert.getAndSet(false)) {
+					final int size = (int)logFileAccess.size();
+					SwingUtilities.invokeLater(new Runnable() {
+						@Override
+						public void run() {
+							fireIntervalAdded(this, size-1, size-1);
+							for (final CyclicModelListener listener: listeners) {
+								listener.linesAdded(size);
+							}
+						}
+					});
+				}
+
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
+
+			log.debug("quitting gracefully");
+		}
+	}
+
+}
--- src/main/java/pl/rychu/jew/gui/LogViewPanel.java
+++ src/main/java/pl/rychu/jew/gui/LogViewPanel.java
@@ -1,23 +1,14 @@
 package pl.rychu.jew.gui;
 
 import java.awt.Component;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import javax.swing.AbstractListModel;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
-import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import pl.rychu.jew.LogFileAccess;
-import pl.rychu.jew.LogFileListener;
 import pl.rychu.jew.LogLineFull;
 
 
@@ -25,8 +16,6 @@ import pl.rychu.jew.LogLineFull;
 public class LogViewPanel extends JList<LogLineFull> implements CyclicModelListener {
 
 	private static final long serialVersionUID = -6731368974272464443L;
-
-	private static final Logger log = LoggerFactory.getLogger(LogViewPanel.class);
 
 	// ---------
 
@@ -60,127 +49,7 @@ public class LogViewPanel extends JList<LogLineFull> implements CyclicModelListe
 		ensureIndexIsVisible(0);
 	}
 
-	// ==================================
-
-	private static class ListModelLog extends AbstractListModel<LogLineFull>
-	 implements LogFileListener {
-
-		private static final long serialVersionUID = 5990060914470736065L;
-
-		private final LogFileAccess logFileAccess;
-
-		private final AtomicBoolean mustNotifyReset = new AtomicBoolean(false);
-
-		private final AtomicBoolean mustNotifyInsert = new AtomicBoolean(false);
-
-		private final List<CyclicModelListener> listeners
-		 = new CopyOnWriteArrayList<>();
-
-		// ------------------
-
-		private ListModelLog(final LogFileAccess logFileAccess) {
-			this.logFileAccess = logFileAccess;
-		}
-
-		public static ListModelLog create(final LogFileAccess logFileAccess
-		 , final LogViewPanel logViewPanel, final InfoPanel infoPanel) {
-			final ListModelLog result = new ListModelLog(logFileAccess);
-
-			logFileAccess.addLogFileListener(result);
-
-			result.addCyclicModelListener(infoPanel);
-			result.addCyclicModelListener(logViewPanel);
-
-			new Thread(result.new ModNotifier()).start();
-
-			return result;
-		}
-
-		public void addCyclicModelListener(final CyclicModelListener lsn) {
-			listeners.add(lsn);
-		}
-
-		public void removeCyclicModelListener(final CyclicModelListener lsn) {
-			listeners.remove(lsn);
-		}
-
-		@Override
-		public int getSize() {
-			return (int)logFileAccess.size();
-		}
-
-		@Override
-		public LogLineFull getElementAt(final int index) {
-			return logFileAccess.getFull(index);
-		}
-
-		@Override
-		public void linesAdded() {
-			mustNotifyInsert.set(true);
-		}
-
-		@Override
-		public void fileWasReset() {
-			mustNotifyReset.set(true);
-		}
-
-		// ------
-
-		private class ModNotifier implements Runnable {
-
-			private final Logger log = LoggerFactory.getLogger(ModNotifier.class);
-
-			// -------
-
-			ModNotifier() {
-				super();
-			}
-
-			// -------
-
-			@Override
-			public void run() {
-				while (!Thread.interrupted()) {
-					if (mustNotifyReset.getAndSet(false)) {
-						log.debug("scheduling file reset");
-						SwingUtilities.invokeLater(new Runnable() {
-							@Override
-							public void run() {
-								fireIntervalRemoved(this, 0, Integer.MAX_VALUE>>1);
-								for (final CyclicModelListener listener: listeners) {
-									listener.listReset();
-								}
-							}
-						});
-					}
-
-					if (mustNotifyInsert.getAndSet(false)) {
-						final int size = (int)logFileAccess.size();
-						SwingUtilities.invokeLater(new Runnable() {
-							@Override
-							public void run() {
-								fireIntervalAdded(this, size-1, size-1);
-								for (final CyclicModelListener listener: listeners) {
-									listener.linesAdded(size);
-								}
-							}
-						});
-					}
-
-					try {
-						Thread.sleep(250);
-					} catch (InterruptedException e) {
-						break;
-					}
-				}
-
-				log.debug("quitting gracefully");
-			}
-		}
-
-	}
-
-	// ------
+	// ==================
 
 	private static class CellRenderer extends DefaultListCellRenderer {
 		private static final long serialVersionUID = 7313136726313412175L;
