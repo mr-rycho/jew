872e0f42c24749e7b748877d0b76fb70b382cc06
rycho <rycho@joasia>
2015-05-08 06:56:26 +0200

    merge threads into one in LogAccessFilter

--- src/main/java/pl/rychu/jew/LogAccessFilter.java
+++ src/main/java/pl/rychu/jew/LogAccessFilter.java
@@ -26,8 +26,7 @@ public class LogAccessFilter implements LogAccess {
 
 	private final GrowingList<Integer> indexB = GrowingListLocked.create(1024);
 
-	private Thread threadForward;
-	private Thread threadBackward;
+	private Thread readerThread;
 
 	// --------------
 
@@ -40,19 +39,15 @@ public class LogAccessFilter implements LogAccess {
 	 , final long startLine) {
 		final LogAccessFilter result = new LogAccessFilter(source, filter);
 
-		result.threadForward = new Thread(result.new SourceReaderForward(startLine));
-		result.threadForward.start();
-		result.threadBackward = new Thread(result.new SourceReaderBackward(startLine));
-		result.threadBackward.start();
+		result.readerThread = new Thread(result.new SourceReaderMultiway(startLine));
+		result.readerThread.start();
 
 		return result;
 	}
 
 	public void dispose() {
-		threadForward.interrupt();
-		threadForward = null;
-		threadBackward.interrupt();
-		threadBackward = null;
+		readerThread.interrupt();
+		readerThread = null;
 	}
 
 	// -------------
@@ -88,12 +83,14 @@ public class LogAccessFilter implements LogAccess {
 
 	// ========================
 
-	private class SourceReaderForward implements Runnable {
+	private class SourceReaderMultiway implements Runnable {
 
-		private long prevSizeF;
+		private long prevMaxIndexF;
+		private long prevMinIndexB;
 
-		SourceReaderForward(final long startLine) {
-			prevSizeF = startLine;
+		SourceReaderMultiway(final long startLine) {
+			prevMaxIndexF = startLine;
+			prevMinIndexB = startLine;
 		}
 
 		@Override
@@ -114,93 +111,60 @@ public class LogAccessFilter implements LogAccess {
 					break;
 				}
 			}
-
+			log.debug("quitting gracefully");
 		}
 
 		private void process() throws BadVersionException {
 			final int version = source.getVersion();
 			if (version != sourceVersion.get()) {
-				log.debug("clearing by F");
-				indexF.clear();
+				log.debug("clearing");
 				indexB.clear();
+				indexF.clear();
 				sourceVersion.set(version);
-				prevSizeF = 0L;
+				prevMaxIndexF = 0L;
+				prevMinIndexB = 0L;
 			}
-			final long sizeF = source.sizeF(version);
-			if (sizeF != prevSizeF) {
-				for (long i=prevSizeF; i<sizeF; i++) {
-					final boolean applies
-					 = filter.needsFullLine()
-					 ? filter.apply(source.getFull(i, version))
-					 : filter.apply(source.get(i, version));
-					if (applies) {
-						indexF.add((int)i);
-					}
-				}
-				prevSizeF = sizeF;
-			}
-		}
 
-	}
+			final int maxSlice = 100;
+			while (true) {
+				final long maxIndexF = source.sizeF(version);
+				final long minIndexB = -source.sizeB(version);
 
-	// ---------
-
-	private class SourceReaderBackward implements Runnable {
-
-		private long prevSizeB;
-
-		SourceReaderBackward(final long startLine) {
-			log.debug("start line: {}", startLine);
-			prevSizeB = startLine;
-		}
-
-		@Override
-		public void run() {
-			log.debug("running");
-			while (!Thread.interrupted()) {
-				try {
-					process();
-				} catch (RuntimeException e) {
-					log.error("error during processing", e);
-				} catch (BadVersionException e) {
-					log.debug("bad version");
-					// ignore; version reset will be handled in next cycle
-				}
-
-				try {
-					Thread.sleep(250);
-				} catch (InterruptedException e) {
+				if (maxIndexF==prevMaxIndexF && minIndexB==prevMinIndexB) {
 					break;
 				}
-			}
 
-			log.debug("quit");
-		}
-
-		private void process() throws BadVersionException {
-			final int version = source.getVersion();
-			if (version != sourceVersion.get()) {
-				log.debug("clearing by B");
-				indexF.clear();
-				indexB.clear();
-				sourceVersion.set(version);
-				prevSizeB = 0L;
-			}
-			final long size = -source.sizeB(version);
-			if (size != prevSizeB) {
-				for (long i=prevSizeB-1; i>=size; i--) {
-					final boolean applies
-					 = filter.needsFullLine()
-					 ? filter.apply(source.getFull(i, version))
-					 : filter.apply(source.get(i, version));
-					if (applies) {
-						indexB.add((int)i);
+				if (maxIndexF != prevMaxIndexF) {
+					final long maxF = Math.min(maxIndexF, prevMaxIndexF+maxSlice);
+					for (long i=prevMaxIndexF; i<maxF; i++) {
+						final boolean applies
+						 = filter.needsFullLine()
+						 ? filter.apply(source.getFull(i, version))
+						 : filter.apply(source.get(i, version));
+						if (applies) {
+							indexF.add((int)i);
+						}
 					}
+					prevMaxIndexF = maxF;
 				}
-				prevSizeB = size;
+
+				if (minIndexB != prevMinIndexB) {
+					final long minB = Math.max(minIndexB, prevMinIndexB-maxSlice);
+					for (long i=prevMinIndexB-1; i>=minB; i--) {
+						final boolean applies
+						 = filter.needsFullLine()
+						 ? filter.apply(source.getFull(i, version))
+						 : filter.apply(source.get(i, version));
+						if (applies) {
+							indexB.add((int)i);
+						}
+					}
+					prevMinIndexB = minB;
+				}
 			}
 		}
 
 	}
+
 
 }
