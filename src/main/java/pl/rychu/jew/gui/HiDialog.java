e27d8fcd7c2e4abb4794676599403fb5be65ee5c
rycho <rycho@joasia>
2015-06-02 18:47:10 +0200

    add cell renderer to hi dialog

--- src/main/java/pl/rychu/jew/gui/HiDialog.java
+++ src/main/java/pl/rychu/jew/gui/HiDialog.java
@@ -1,12 +1,15 @@
 package pl.rychu.jew.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -15,6 +18,8 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 import pl.rychu.jew.gui.hi.HiConfig;
 import pl.rychu.jew.gui.hi.HiConfigEntry;
@@ -40,6 +45,7 @@ public class HiDialog extends JDialog {
 		final DefaultListModel<HiConfigEntry> model = createModel(hiConfig);
 		final JList<HiConfigEntry> jList = new JList<HiConfigEntry>(model);
 		jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jList.setCellRenderer(new CellRenderer());
 		cp.add(jList, BorderLayout.CENTER);
 
 		final JPanel buttonPanel = new JPanel();
@@ -68,4 +74,44 @@ public class HiDialog extends JDialog {
 		return result;
 	}
 
+	// =======================
+
+	private static class CellRenderer extends DefaultListCellRenderer {
+
+		private static final long serialVersionUID = 5002424923342503398L;
+
+		private static final Border SEL_BORDER = new LineBorder(Color.BLACK, 1);
+
+		@Override
+		public Component getListCellRendererComponent(final JList<?> list, final Object value
+		 , final int index, final boolean isSelected, final boolean cellHasFocus) {
+			return getListCellRendererComponentSuper(list, value, index, isSelected, cellHasFocus);
+		}
+
+		public Component getListCellRendererComponentSuper(final JList<?> list
+		 , final Object valueObj, final int index
+		 , final boolean isSelected, final boolean cellHasFocus) {
+
+			final HiConfigEntry value = (HiConfigEntry)valueObj;
+
+			setComponentOrientation(list.getComponentOrientation());
+
+			setBackground(new Color(value.getColorB()));
+			setForeground(new Color(value.getColorF()));
+
+			setIcon(null);
+			setText((value == null) ? "" : value.getRegexp());
+
+			setEnabled(list.isEnabled());
+			setFont(list.getFont());
+
+			Border border = noFocusBorder;
+			if (cellHasFocus && isSelected) {
+				border = SEL_BORDER;
+			}
+			setBorder(border);
+
+			return this;
+		}
+	}
 }
