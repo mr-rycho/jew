92e5ec2ffc716e8ed428b211abf93e97244937b0
rycho <rycho@joasia>
2015-06-11 16:37:35 +0200

    add '... 50 more' to stack

--- src/main/java/pl/rychu/jew/linedec/LineDecoderStack.java
+++ src/main/java/pl/rychu/jew/linedec/LineDecoderStack.java
@@ -11,20 +11,36 @@ public class LineDecoderStack implements LineDecoder {
 	private static final Pattern PATTERN_STACK
 	 = Pattern.compile("^[ \\t]*at[ \\t]+([^(]+).*$");
 
+	private static final Pattern PATTERN_MORE
+	 = Pattern.compile("^[ \\t]*...[ \\t]+[0-9]+[ \\t]+more$");
+
 	// -----------
 
 	@Override
 	public LogLine decode(final long filePos, final String line
 	 , final int length, final LogLine prevLine) {
 		final Matcher matcherStack = PATTERN_STACK.matcher(line);
+		String classnameRaw = null;
+		boolean matchesAny = false;
 		if (matcherStack.matches()) {
-			final String classname = matcherStack.group(1);
+			classnameRaw = matcherStack.group(1);
+			matchesAny = true;
+		} else {
+			final Matcher matcherMore = PATTERN_MORE.matcher(line);
+			if (matcherMore.matches()) {
+				classnameRaw = null;
+				matchesAny = true;
+			}
+		}
+		if (matchesAny) {
+			final String classname
+			 = classnameRaw!=null ? LogElemsCache.getOrPutLogger(classnameRaw) : null;
 			final String threadName = prevLine!=null ? prevLine.getThreadName() : null;
 			LogLineStack logLineStack = LineDecoderUtil.getLogLineStack(prevLine);
 			int stackLineTotal = logLineStack!=null ? logLineStack.getStackLineTotal()+1 : 0;
 			int stackLineInCause = logLineStack!=null ? logLineStack.getStackLineInCause()+1 : 0;
 			return LogLineStack.create(filePos, length, threadName
-			 , LogElemsCache.getOrPutLogger(classname), stackLineTotal, stackLineInCause);
+			 , classname, stackLineTotal, stackLineInCause);
 		} else {
 			return null;
 		}
