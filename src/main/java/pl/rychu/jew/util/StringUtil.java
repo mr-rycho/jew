7e0d83c034da8150d1cc8239d5baa247f45adc58
rycho <rycho@joasia>
2015-07-25 11:11:05 +0200

    extend join strings method

--- src/main/java/pl/rychu/jew/util/StringUtil.java
+++ src/main/java/pl/rychu/jew/util/StringUtil.java
@@ -7,17 +7,25 @@ public class StringUtil {
 	private StringUtil() {}
 
 	public static <T> String join(T[] elems) {
-		if (elems==null || elems.length==0) {
-			return "";
-		}
+		return join(elems, null, null, null, null, ", ");
+	}
+
+	public static <T> String join(T[] elems, String listStart, String listEnd
+	 , String elemStart, String elemEnd, String joiner) {
 		StringBuilder sb = new StringBuilder();
-		for (Object obj: elems) {
-			String objStr = obj==null ? "[null]" : obj.toString().trim();
-			if (sb.length() > 0) {
-				sb.append(", ");
+		sb.append(listStart!=null ? listStart : "");
+		if (elems != null) {
+			boolean first = true;
+			for (Object obj: elems) {
+				String objStr = obj==null ? "[null]" : obj.toString().trim();
+				if (first) first = false;
+				else sb.append(joiner!=null ? joiner : "");
+				sb.append(elemStart!=null ? elemStart : "");
+				sb.append(objStr);
+				sb.append(elemEnd!=null ? elemEnd : "");
 			}
-			sb.append(objStr);
 		}
+		sb.append(listEnd!=null ? listEnd : "");
 		return sb.toString();
 	}
 
